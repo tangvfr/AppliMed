e97200037e9db6e1f81332e528569ba4c7c7070b
BuildTools <unconfigured@null.spigotmc.org>
2023-04-12 17:01:12 +0200

    Test show list

--- app/src/main/java/fr/tangv/applimed/activity/FamilleListFragment.java
+++ app/src/main/java/fr/tangv/applimed/activity/FamilleListFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import fr.tangv.applimed.R;
 import fr.tangv.applimed.databinding.FragmentFamilleListBinding;
 
 public class FamilleListFragment extends Fragment {
@@ -19,12 +20,30 @@ public class FamilleListFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        binding = FragmentFamilleListBinding.inflate(inflater, container, false);
+        this.binding = FragmentFamilleListBinding.inflate(inflater, container, false);
+        //titre
+        this.binding.topPad.setTitleBarName(
+                this.getString(R.string.fam_title)
+        );
+        //text a gauche
+        this.binding.topPad.setFirstBarName(
+                this.getString(R.string.fam_code)
+        );
+        //text a droite
+        this.binding.topPad.setSecondBarName(
+                this.getString(R.string.fam_lib)
+        );
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+
+        this.binding.familleList.setIsEmpty(false);
+        this.binding.familleList.setEmptyMessage("Testage");
+
 
 
     }
